d4e45ee8c644687b60ee63cc0f3d650edccae775
Ben Heyman <benheyman1@gmail.com>
2023-02-27 15:31:18 +0000

    Created controller for sharks and added 7 CRUD methods

---
+++ app/controllers/sharks_controller.rb
@@ -0,0 +1,47 @@
+class SharksController < ApplicationController
+  before_action :set_list, only:[:edit, :show, :update, :destroy]
+
+  def index
+    @sharks = Shark.all
+  end
+
+  def show; end
+
+  def new
+    @shark = Shark.new
+  end
+
+  def create
+    @shark = Shark.new(shark_params)
+    if @shark.save
+      redirect_to shark_path(@shark)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @shark.update(shark_params)
+      redirect_to shark_path(@shark)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @shark.destroy
+    redirect_to sharks_path, status: see_other
+  end
+
+  private
+
+  def set_shark
+    @shark = Shark.find(params[:id])
+  end
+
+  def shark_params
+    params.require(:shark).permit(:name, :age, :type, :description, :price, :diet, :picture_url)
+  end
+end
