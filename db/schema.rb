57f47481b7628093f612735c86c00f5aea08dd4f
Ben Heyman <benheyman1@gmail.com>
2023-02-27 14:43:51 +0000

    Created shark model and performed migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_141152) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_143426) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "sharks", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "type"
+    t.string "description"
+    t.integer "price"
+    t.string "diet"
+    t.string "picture_url"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sharks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +42,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_141152) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "sharks", "users"
 end
