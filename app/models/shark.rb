1b881843c7f176d1f8541d2c0ccbb3ab2b4eda76
Ben Heyman <benheyman1@gmail.com>
2023-02-27 14:53:46 +0000

    Added validations and associations to shark model

--- app/models/shark.rb
+++ app/models/shark.rb
@@ -1,2 +1,7 @@
 class Shark < ApplicationRecord
+  belongs_to :user
+
+  validates_presence_of :name, :age, :type, :description, :price, :diet, :picture_url
+  validates :age, numericality: { only_integer: true }
+  validates :price, numericality: true
 end
