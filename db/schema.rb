3fdda2d6b11da2ca5b689f17bce511540643d4be
Ben Heyman <benheyman1@gmail.com>
2023-02-27 16:31:48 +0000

    Added booking model and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_143426) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_162718) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id"
+    t.bigint "shark_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shark_id"], name: "index_bookings_on_shark_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "sharks", force: :cascade do |t|
     t.string "name"
@@ -42,5 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_143426) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "sharks"
+  add_foreign_key "bookings", "users"
   add_foreign_key "sharks", "users"
 end
