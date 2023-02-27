5c71c7ddee9f8b9586cf3cac82d79ea5a7134ff5
Ben Heyman <benheyman1@gmail.com>
2023-02-27 16:37:24 +0000

    Added booking validations and updated existing validations

--- app/models/shark.rb
+++ app/models/shark.rb
@@ -1,5 +1,7 @@
 class Shark < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  has_many :users, through: :bookings
 
   validates_presence_of :name, :age, :type, :description, :price, :diet, :picture_url
   validates :age, numericality: { only_integer: true }
